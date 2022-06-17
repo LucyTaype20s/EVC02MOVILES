3c8238f491c17742a113af8c6dee06b8d338328c
LucyTaype20s <vpo.ltaype@gmail.com>
2022-06-17 15:16:37 -0500

    Examen terminadogit add .git add .git add .git add .

--- app/src/main/java/com/example/evc02_ap/MainActivity.java
+++ app/src/main/java/com/example/evc02_ap/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,7 +18,11 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView lista;
     private EditText txt_nombre,txt_apellido,txt_rptaNombre,txt_rptaApellido;
-    private TextView tv_bienvenida,tv_respuesta;
+    private TextView tv_bienvenida,tv_respuesta,tv_item;
+
+    private RadioGroup grupo_rb;
+    private RadioButton rb_1,rb_2,rb_3;
+
     float numero1=0.0f;
     float numero2=0.0f;
     String operacion="";
@@ -41,7 +47,33 @@ public class MainActivity extends AppCompatActivity {
         tv_respuesta=findViewById(R.id.tvRespuesta);
 
 
+        tv_item=findViewById(R.id.tv_itemList);
+        grupo_rb=findViewById(R.id.rg_grupo);
+        rb_1=findViewById(R.id.rb_1);
+        rb_2=findViewById(R.id.rb_2);
+        rb_3=findViewById(R.id.rb_3);
 
+
+}
+
+public void onClick(View view){
+if(view.getId()==R.id.btn_ejercicio3){
+    validar();
+}
+}
+private void validar(){
+        String cad=" a Grados Fahrenheit: \n";
+        if(rb_1.isChecked()){
+            cad+=""+Fahrenheit(32);
+        }if(rb_2.isChecked()){
+        cad+=""+Fahrenheit(13);
+    }if(rb_3.isChecked()){
+        cad+=""+Fahrenheit(21);
+    }
+        tv_item.setText(cad);
+}
+private double Fahrenheit(double celsius){
+        return (celsius*(9/5))+32;
 }
 
 
